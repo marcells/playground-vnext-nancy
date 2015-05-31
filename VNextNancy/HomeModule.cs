17eab0f097f9cd6271b7e6b34dbb8f4702f64d37
Marcell Spies <marcells@gmx.de>
2015-06-01 01:47:19 +0200

    Inital commit.

---
+++ VNextNancy/HomeModule.cs
@@ -0,0 +1,12 @@
+namespace VNextNancy
+{
+    using Nancy;
+    
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = _ => "Hello World";
+        }
+    }
+}
