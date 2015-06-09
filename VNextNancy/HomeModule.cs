8d9db77a556f2c9aac83ce8b292e835bf4440159
Marcell Spies <marcells@gmx.de>
2015-06-10 00:10:33 +0200

    Added some sample routes.

--- VNextNancy/HomeModule.cs
+++ VNextNancy/HomeModule.cs
@@ -1,4 +1,5 @@
 using Nancy;
+using System.Threading.Tasks;
 
 namespace VNextNancy
 {
@@ -7,6 +8,13 @@ namespace VNextNancy
         public HomeModule()
         {
             Get["/"] = _ => "Hello World";
+            
+            Get["/hi/{name}"] = parameters => $"Hello {parameters.name}!";
+            
+            Get["/async", runAsync: true] = async (_, token) => {
+                await Task.Delay(5000);
+                return "Hello World delayed";
+            };
         }
     }
 }
