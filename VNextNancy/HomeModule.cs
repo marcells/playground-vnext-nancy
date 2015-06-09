967ac225311e6e00eb0c21128dd7e8fa1a708622
Marcell Spies <marcells@gmx.de>
2015-06-10 00:47:20 +0200

    Make routing overview.

--- VNextNancy/HomeModule.cs
+++ VNextNancy/HomeModule.cs
@@ -7,12 +7,18 @@ namespace VNextNancy
     {
         public HomeModule()
         {
-            Get["/"] = _ => "Hello World";
+            Get["/"] = _ => View["Index"];
+            
+            Get["/hi"] = _ => "Hello World";
             
             Get["/hi/{name}"] = parameters => $"Hello {parameters.name}!";
             
+            Get["/products/{id:int}"] = parameters => $"Product with id {parameters.id}!";
+            
+            Get["/products/between/{left:int}/and/{right:int}"] = parameters => $"Products between {parameters.left} and {parameters.right}!";
+            
             Get["/async", runAsync: true] = async (_, token) => {
-                await Task.Delay(5000);
+                await Task.Delay(3000);
                 return "Hello World delayed";
             };
         }
--- VNextNancy/Startup.cs
+++ VNextNancy/Startup.cs
@@ -1,13 +1,40 @@
 using Microsoft.AspNet.Builder;
+using Nancy;
 using Nancy.Owin;
+using Microsoft.Framework.Runtime;
 
 namespace VNextNancy
 {
     public class Startup
     {
+        internal static IApplicationEnvironment Environment { get; private set; }
+
+        public Startup(IApplicationEnvironment env)
+        {
+            Environment = env;
+        }
+        
         public void Configure(IApplicationBuilder app)
         {
             app.UseOwin(x => x.UseNancy());
+        }
+    }
+    
+    public class vNextRootPathProvider : IRootPathProvider
+    {
+        private string BasePath = Startup.Environment.ApplicationBasePath;
+    
+        public string GetRootPath()
+        {
+            return BasePath;
+        }
+    }
+    
+    public class CustomBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override IRootPathProvider RootPathProvider
+        {
+            get { return new vNextRootPathProvider(); }
         }
     }
 }
