f536abd07e1f9ec79e901d5f3ec2ebffc654d8e9
Marcell Spies <marcells@gmx.de>
2015-06-11 00:00:50 +0200

    Added some pipelining stuff.

--- VNextNancy/HomeModule.cs
+++ VNextNancy/HomeModule.cs
@@ -1,5 +1,7 @@
+using System;
 using Nancy;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace VNextNancy
 {
@@ -7,20 +9,40 @@ namespace VNextNancy
     {
         public HomeModule()
         {
+            Before += ctx =>
+            {
+                WriteLine($"Process Route: {ctx.ResolvedRoute.Description.Path}");
+                return null;
+            };
+            
+            After += ctx =>
+            {
+                WriteLine($"Finished Route: {ctx.ResolvedRoute.Description.Path}");
+            };
+            
+            OnError += (ctx, ex) =>
+            {
+                WriteLine($"Error in Route: {ctx.ResolvedRoute.Description.Path}");
+                return null;
+            };
+
             Get["/"] = _ => View["Index"];
-            
+
             Get["/hi"] = _ => "Hello World";
-            
+
             Get["/hi/{name}"] = parameters => $"Hello {parameters.name}!";
-            
+
             Get["/products/{id:int}"] = parameters => $"Product with id {parameters.id}!";
-            
+
             Get["/products/between/{left:int}/and/{right:int}"] = parameters => $"Products between {parameters.left} and {parameters.right}!";
-            
-            Get["/async", runAsync: true] = async (_, token) => {
+
+            Get["/async", runAsync: true] = async (_, token) =>
+            {
                 await Task.Delay(3000);
                 return "Hello World delayed";
             };
+            
+            Get["/error"] = _ => { throw new Exception("Ooops, something went wrong!"); };
         }
     }
 }
